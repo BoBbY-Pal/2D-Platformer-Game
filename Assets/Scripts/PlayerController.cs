ca20c2976b5c9729e3e87cbd267fbf0ef6cd8e14
BoBbY-Pal <officialbobby0001@gmail.com>
2022-01-20 11:48:45 +0530

    TileMap
    setup tilemap pallets.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,48 +16,62 @@ public class PlayerController : MonoBehaviour
     }
 
     void Update()
-    {   //Detecting user inputs
+    {   //  Detecting user inputs
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Jump");
 
         //Function calling
         PlayerMovementAnimation(horizontal, vertical);
         MoveCharacter(horizontal, vertical);
+
+        if(isPlayerGrounded()) { animator.SetBool("isGrounded", true ); }
+        else { animator.SetBool("isGrounded", false ); }
     }
 
     private void MoveCharacter(float horizontal,float vertical)
-    {  //Move character horizotally
+    {  //   Move character horizotally
        Vector3 position = transform.position;
        position.x += horizontal * speed * Time.deltaTime;
        transform.position = position;
 
-       //Jump
+       //   Jump
         if(isPlayerGrounded() && Input.GetKeyDown(KeyCode.Space)) {
             _rigidbody2D.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
         }
     }
-    private bool isPlayerGrounded() 
-    {   
-        RaycastHit2D raycasthit2d = Physics2D.BoxCast(_capsuleCollider2d.bounds.center, _capsuleCollider2d.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
-        Debug.Log(raycasthit2d.collider);
-        return raycasthit2d.collider != null;
-    }
-    private void PlayerMovementAnimation(float horizontal,float vertical)   
-    {   //Horizontal Movement Animation
-        animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        Vector3 scale = transform.localScale;
-        if(horizontal < 0) {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        } 
-        else if(horizontal > 0) {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
 
-        //JUMP
-        if(isPlayerGrounded() && Input.GetKeyDown(KeyCode.Space))
-        {
-            animator.SetTrigger("Jump");
-        };
+    private bool isPlayerGrounded()         //  Checks player is on ground or not
+    {   
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(_capsuleCollider2d.bounds.center, _capsuleCollider2d.bounds.size, 0f, Vector2.down, .1f, platformLayerMask);
+        Color rayColor;
+        if(raycastHit2d.collider != null) {
+            rayColor = Color.cyan;
+        } else {
+            rayColor = Color.red;
+        }
+        Debug.DrawRay(_capsuleCollider2d.bounds.center + new Vector3(_capsuleCollider2d.bounds.extents.x, 0), Vector2.down * (_capsuleCollider2d.bounds.extents.y + .1f), rayColor);
+        Debug.DrawRay(_capsuleCollider2d.bounds.center - new Vector3(_capsuleCollider2d.bounds.extents.x, 0), Vector2.down * (_capsuleCollider2d.bounds.extents.y + .1f), rayColor);
+        Debug.DrawRay(_capsuleCollider2d.bounds.center - new Vector3(_capsuleCollider2d.bounds.extents.x, _capsuleCollider2d.bounds.extents.y + .1f), Vector2.right * (_capsuleCollider2d.bounds.extents.x), rayColor);
+        Debug.Log(raycastHit2d.collider);
+        return raycastHit2d.collider != null;
+    }
+
+    private void PlayerMovementAnimation(float horizontal,float vertical)   
+    {   //  Horizontal Movement Animation
+            animator.SetFloat("Speed", Mathf.Abs(horizontal));
+            Vector3 scale = transform.localScale;
+            if(horizontal < 0) {
+                scale.x = -1f * Mathf.Abs(scale.x);
+            } 
+            else if(horizontal > 0) {
+                scale.x = Mathf.Abs(scale.x);
+            }
+            transform.localScale = scale;
+    
+        //  JUMP
+            if(isPlayerGrounded() && Input.GetKeyDown(KeyCode.Space)) {
+                animator.SetTrigger("Jump");
+            }
+        
     }
 }
